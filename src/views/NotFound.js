29777d6922a0501d6d3caf1669a87c4920ccb934
MariaCiffu <maria.cifuentes554@comunidadunir.net>
2023-12-17 15:52:34 +0100

    notFound

--- src/views/NotFound.js
+++ src/views/NotFound.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import "../styles/styles.css"
 
 function NotFound() {
     return (
         <div>
-            <h1>404 - Página no encontrada</h1>
+            <img className="not-found" src="https://static.vecteezy.com/system/resources/thumbnails/008/255/803/small/page-not-found-error-404-system-updates-uploading-computing-operation-installation-programs-system-maintenance-a-hand-drawn-layout-template-of-a-broken-robot-illustration-vector.jpg" alt="not found"/>
         </div>
     );
 }

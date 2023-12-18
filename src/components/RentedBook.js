9f3332863555586cecc0d6e4592c8b3cae32544a
MariaCiffu <maria.cifuentes554@comunidadunir.net>
2023-12-18 13:37:54 +0100

    suma de días

--- src/components/RentedBook.js
+++ src/components/RentedBook.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, {useContext} from "react";
+import {CiCirclePlus} from "react-icons/ci";
+import {LibraryContext} from "../context/LibraryContext";
+import {useNavigate} from "react-router";
 
+export const RentedBook = ({id, nombre, portada, diasPrestamo, libro}) => {
+    const { rentBooks, setRentBooks } = useContext(LibraryContext);
+    const navigate = useNavigate();
+    const addDays =() => {
+        const libroEnc = rentBooks.find((item) => item.id === id);
+        if(libroEnc){
+            setRentBooks(rentBooks.map((item) => (item.id === id ? {...libro, diasPrestamo : diasPrestamo + 1} : item)));
+        }
+    }
 
-export const RentedBook = ({ nombre, portada, diasPrestamo}) => {
     return (
         <div className="libros-alquilados">
             <img src={portada} alt={portada}/>
-            <h3>Nombre : {nombre}</h3>
-            <p>{diasPrestamo}</p>
+            <h3>{nombre}</h3>
+            <p className="periodo-prestamo">Periodo prestamo: {diasPrestamo} días</p>
+            <CiCirclePlus className="add-button" onClick={() => addDays()}/>
         </div>
     );
 }
--- src/views/RentedBooks.js
+++ src/views/RentedBooks.js
@@ -1,22 +1,21 @@
 import {LibraryContext} from "../context/LibraryContext";
 import React, {useContext} from "react";
 import {RentedBook} from "../components/RentedBook";
+import "../styles/rentedBooksStyle.css"
 
 export const RentedBooks = () => {
     const { rentBooks } = useContext(LibraryContext);
 
     return (
-        <div>
-            {
-                rentBooks.map((book, index) => (
-                    <RentedBook
-                        key={index}
-                        nombre={book.nombre}
-                        portada={book.portada}
-                        diasPrestamo= "30"
-                    />
-                ))
-            }
-        </div>
-    );
+        rentBooks.map((book, index) => (
+            <RentedBook
+                key={index}
+                id={book.id}
+                nombre={book.nombre}
+                portada={book.portada}
+                diasPrestamo={book.diasPrestamo}
+                libro = {book}
+            />
+        ))
+    )
 }

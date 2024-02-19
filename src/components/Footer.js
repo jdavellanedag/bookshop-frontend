7204208509e8ce9d56bb7ce57d5651ce01b255d3
MariaCiffu <maria.cifuentes554@comunidadunir.net>
2024-02-19 18:59:17 +0100

    Cambio nombre

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export const Footer = () => {
     return (
         <footer className="footer">
-            <p>© La librería de Ohara</p>
+            <p>© La biblioteca de Ohara</p>
         </footer>
     );
 }
